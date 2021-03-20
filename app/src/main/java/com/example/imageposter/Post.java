828d54d62919b85f41e38b7b7f7ec0655533442d
andrey548 <andreyverh@icloud.com>
2021-03-20 12:36:25 -0400

    new commit

--- app/src/main/java/com/example/imageposter/Post.java
+++ app/src/main/java/com/example/imageposter/Post.java
@@ -32,6 +32,10 @@ public class Post extends ParseObject {
         return getParseUser(KEY_USER);
     }
 
+    public String getUsername() {
+        return getParseUser(KEY_USER).getUsername();
+    }
+
     public void setUser(ParseUser user) {
         put(KEY_USER, user);
     }
